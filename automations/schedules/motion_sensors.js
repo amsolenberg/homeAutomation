1f1f1f6c732bafcb15f4caa1beaef1e58acbb381
Aaron Solenberg <amsolenberg@gmail.com>
2025-06-02 22:36:17 -0400

    Converted `toggleMotionSensors` to external helper function in `utils.js`

--- automations/schedules/motion_sensors.js
+++ automations/schedules/motion_sensors.js
@@ -1,6 +1,4 @@
-import cron from 'node-cron';
-import { callService } from '../../lib/ha-rest.js';
-import { getTimestamp } from '../../lib/utils.js';
+import { cronSchedule } from '../../lib/utils.js';
 
 const generalRooms = [
     'input_boolean.toggle_motion_kitchen',
@@ -24,22 +22,8 @@ const bedrooms = [
     'input_boolean.toggle_motion_kaels_bedroom'
 ];
 
-function toggleMotionSensors(cronSchedule, roomArray, service, label = 'unnamed task') {
-    console.log(`${getTimestamp()} [Scheduler] Scheduled - ${label}`);
-    cron.schedule(cronSchedule, async () => {
-        try {
-            await callService('switch', service, {
-                entity_id: roomArray
-            });
-            console.log(`${getTimestamp()} [Scheduler] '${label}' ran successfully.`);
-        } catch (e) {
-            console.error(`${getTimestamp()} [Scheduler] '${label}' failed:`, e.message || e);
-        }
-    });
-}
+cronSchedule('0 5 * * *', generalRooms, 'switch', 'turn_on', 'Turn on general rooms motion sensors at 0500');
 
-toggleMotionSensors('0 5 * * *', generalRooms, 'turn_on', 'Turn on general room motion sensors at 0500');
+cronSchedule('0 9 * * *', bedrooms, 'switch', 'turn_on', 'Turn on bedroom motion sensors at 0900');
 
-toggleMotionSensors('0 9 * * *', bedrooms, 'turn_on', 'Turn on bedroom motion sensors at 0900');
-
-toggleMotionSensors('0 21 * * *', bedrooms, 'turn_off', 'Turn off bedroom motion sensors at 2100');
+cronSchedule('0 21 * * *', bedrooms, 'switch', 'turn_off', 'Turn off bedroom motion sensors at 2100');
