6005bdfabf44ac4b47ae8f6d99c8488c8961d7d7
Aaron Solenberg <amsolenberg@gmail.com>
2025-06-02 22:40:38 -0400

    Removed unused variables

--- config.js
+++ config.js
@@ -10,15 +10,3 @@ export const NTFY = {
     BASE_URL: process.env.NTFY_URL,
     TOKEN: process.env.NTFY_TOKEN
 };
-
-export const ENTITIES = {
-    AC_TOGGLE: 'input_boolean.toggle_status_main_bedroom_portable_ac',
-    AC_OVERRIDE: 'input_boolean.toggle_status_portable_ac_override',
-    AC_SWITCH: 'switch.outlet_main_bedroom_portable_ac'
-};
-
-export const SCHEDULE = {
-    AC_ON_HOUR: 19,
-    AC_OFF_HOUR_WEEKDAY: 8,
-    AC_OFF_HOUR_WEEKEND: 9
-};
