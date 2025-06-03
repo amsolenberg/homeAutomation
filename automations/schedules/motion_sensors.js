39e2b9b1c959d12baab5e28ab2ab8d0ace0ce503
Aaron Solenberg <amsolenberg@gmail.com>
2025-06-03 08:15:16 -0400

    Corrected domain from `switch` to `input_boolean` and corrected `_gameroom` to `_office`

--- automations/schedules/motion_sensors.js
+++ automations/schedules/motion_sensors.js
@@ -4,7 +4,7 @@ const generalRooms = [
     'input_boolean.toggle_motion_kitchen',
     'input_boolean.toggle_motion_dining_room',
     'input_boolean.toggle_motion_garage',
-    'input_boolean.toggle_motion_gameroom',
+    'input_boolean.toggle_motion_office',
     'input_boolean.toggle_motion_upstairs_bathroom',
     'input_boolean.toggle_motion_upstairs_hallway',
     'input_boolean.toggle_motion_living_room',
@@ -22,8 +22,8 @@ const bedrooms = [
     'input_boolean.toggle_motion_kaels_bedroom'
 ];
 
-cronSchedule('0 5 * * *', generalRooms, 'switch', 'turn_on', 'Turn on general rooms motion sensors at 0500');
+cronSchedule('0 5 * * *', generalRooms, 'input_boolean', 'turn_on', 'Turn on general rooms motion sensors at 0500');
 
-cronSchedule('0 9 * * *', bedrooms, 'switch', 'turn_on', 'Turn on bedroom motion sensors at 0900');
+cronSchedule('0 9 * * *', bedrooms, 'input_boolean', 'turn_on', 'Turn on bedroom motion sensors at 0900');
 
-cronSchedule('0 21 * * *', bedrooms, 'switch', 'turn_off', 'Turn off bedroom motion sensors at 2100');
+cronSchedule('0 21 * * *', bedrooms, 'input_boolean', 'turn_off', 'Turn off bedroom motion sensors at 2100');
